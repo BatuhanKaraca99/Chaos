778ce2b6591bb39cab57e946752d1ec2d3c8c2d3
Batuhan KARACA <b.karaca.1999@gmail.com>
2023-11-14 18:48:11 +0300

    Idle,Walk,Run,Flip Direction

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,13 +7,81 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     public float walkSpeed = 5f;
-    Vector2 moveInput; 
-    public bool IsMoving { get; private set; }
+    public float runSpeed = 8f;
+    Vector2 moveInput;
+
+    public float CurrentMoveSpeed { get 
+        {
+            if (IsMoving)
+            {
+                if (IsRunning)
+                {
+                    return runSpeed;
+                }
+                else
+                {
+                    return walkSpeed;
+                }
+            }
+            else
+            {
+                //Idle
+                return 0;
+            }
+        } }
+
+    [SerializeField]
+    private bool _isMoving = false;
+
+    public bool IsMoving { get
+        {
+            return _isMoving;
+        } private set
+        {
+            _isMoving = value;
+            animator.SetBool("isMoving", value);
+        }
+    }
+
+    [SerializeField]
+    private bool _isRunning = false;
+
+    public bool IsRunning
+    {
+        get
+        {
+            return _isRunning;
+        }
+        set
+        {
+            _isRunning = value;
+            animator.SetBool("isRunning",value);
+        }
+    }
+
+    public bool _isFacingRight = true;
+    public bool IsFacingRight { 
+        get { 
+            return _isFacingRight; 
+        }
+        private set
+        {
+            if (_isFacingRight != value)
+            {
+                //flip local scale
+                transform.localScale *= new Vector2(-1, 1);
+            }
+            _isFacingRight = value;
+        }
+    }
+
     Rigidbody2D rb;
+    Animator animator;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Start is called before the first frame update
@@ -30,7 +98,7 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate()
     {
-        rb.velocity = new Vector2(moveInput.x * walkSpeed,rb.velocity.y);
+        rb.velocity = new Vector2(moveInput.x * CurrentMoveSpeed,rb.velocity.y);
     }
 
     public void OnMove(InputAction.CallbackContext context)
@@ -38,5 +106,32 @@ public class PlayerController : MonoBehaviour
         moveInput = context.ReadValue<Vector2>();
 
         IsMoving = moveInput != Vector2.zero;
+
+        SetFacingDirection(moveInput);
+    }
+
+    private void SetFacingDirection(Vector2 moveInput)
+    {
+        if(moveInput.x > 0 && !IsFacingRight)
+        {
+            //Right
+            IsFacingRight = true;
+        }
+        else if(moveInput.x < 0 && IsFacingRight)
+        {
+            //Left
+            IsFacingRight= false;
+        }
+    }
+
+    public void OnRun(InputAction.CallbackContext context)
+    {
+        if (context.started)
+        {
+            IsRunning = true;
+        } else if (context.canceled)
+        {
+
+        }
     }
 }
